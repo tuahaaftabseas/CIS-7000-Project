e1722b29f7f19680d4ccedfe3703d6d7d6fc2371
tuahaaftabseas <tuaha@seas.upenn.edu>
2022-12-10 22:48:18 -0500

    Added comments

--- NFT_Auction.sol
+++ NFT_Auction.sol
@@ -11,48 +11,59 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 // Below contract adapted from deployed HW0 contract: https://goerli.etherscan.io/address/0x345565c62EFB2859769b6Ee887577123C550a6Ff
 contract GlobalWarmingNFTCollection is ERC721URIStorage, Ownable {
 
-    // Following string can be used for off chain NFT storage
+    // Similar string can be used as a reference for off chain storage of NFT related data like videos, images
     // string private URI = "ipfs://QmZT2jjJYKQ1SbeEo4BzaSwghJyxKqjMoCvn64puyGXQCM";
     string private URI = ""; 
-   
+
+    // Token number to keep track of the tokens issues for this NFT
+    // An NFT is recognized by the token contract address and token ID
     uint256 private tokenNo;
 
+
+    // Second argument is the symbol used for the NFT
     constructor() ERC721("Global Warming NFT Collection", "GW_NFT") {
-        
-        AddressBook[msg.sender] = "Auction Contract";
-    
+
     }
 
-    function mintNFT() public isInAddressBook(msg.sender) returns (uint) {
+    function mintNFT() public onlyOwner returns (uint) {
         require (tokenNo + 1 > tokenNo);
         tokenNo++;
         
         uint256 newItemId = tokenNo;
         
+        // Creates a new token with the item ID and transfers ownership to the auction contract
         _mint(msg.sender, newItemId);
+
+        // This sets the URI for the token. The URI can be used to access off-chain resources linked with the token.
         _setTokenURI(newItemId, URI);
 
+        // Returns tokenNo to the auction contract so the auction contract can keep track of the current NFT
         return tokenNo;     
     }
 
-    function latestIssuedTokenNo() public returns (uint) {
+    function latestIssuedTokenNo() public view onlyOwner returns (uint) {
         return tokenNo;
     }
 
-    mapping (address => string) public AddressBook ;
-
-
-    modifier isInAddressBook(address addr) {
-        require (bytes(AddressBook[addr]).length > 0);
-        _;
-    }    
 }
 
 
 
-// Below contract taken from: HW1
+// Below contract taken from HW1 and modified
 
 
+/*
+    1) Owner deploys auction contract. Initially auction is closed.
+    2) Auction contract deploys NFT contract in its constructor
+    3) Owner calls auction function to mint NFT token when the auction is closed
+    4) Owner becomes the owner of the NFT
+    5) Users bid. Only the highest bid and bidder is kept. Previous bidder is refunded
+    6) Owner closes auction
+    7) Owner payouts the NFT to the winner
+    8) Owner withdraw funds (In case a percentage of the winning bid amount needs to be kept)
+    8) Restart from point 2
+
+*/
 contract Auction {
     address payable public owner;
 
@@ -74,23 +85,23 @@ contract Auction {
         highestBidder = owner;
     }
     
-    function mint_NFT_for_Auction() public isClosed  {  
-        require(nftTokenId == 0); // This is set to 0 when the winner has been payed out
+    function nftCollectionAddress() public view onlyOwner returns (address) {
+        return address(nftCollection);
+    }
+
+    function mint_NFT_for_Auction() public onlyOwner isClosed  {  
+        // This is set to 0 when the winner has been payed out
+        require(nftTokenId == 0); 
 
         // Minting new NFT token for auction using the GlobalWarmingNFTCollection. 
         // The auction is transffered the ownership of the minted token.
         // The auction keeps track of the latest minted auction using the nftTokenId
         // This token ID is used to tranfer the current token for auction to the highest bidder
         nftTokenId = nftCollection.mintNFT(); 
-    
     }
 
-    function latestIssuedTokenNo() public returns (uint) {
+    function latestIssuedTokenNo() public view onlyOwner returns (uint) {
         return nftCollection.latestIssuedTokenNo();
-    }
-
-    function nftmanager() external view returns (address) {
-        return address(nftCollection);
     }
 
     modifier onlyOwner() {
@@ -130,6 +141,7 @@ contract Auction {
          */
         endTime = block.timestamp;
 
+        // Highest bid reset to 0
         highestBid = 0;
     }
 
@@ -142,7 +154,7 @@ contract Auction {
             "New bid should be higher than current highest bid"
         );
 
-        // Check for reentrancy vulnerability here
+        // ===> Check for reentrancy vulnerability here
         payable(highestBidder).transfer(highestBid);
 
         highestBid = msg.value;
@@ -159,9 +171,21 @@ contract Auction {
         nftTokenId = 0; // Reseting token ID
     }
 
-    // Verify logic for below function
-    function withdrawFunds() public onlyOwner isClosed
-    {
-        payable(msg.sender).transfer(address(this).balance);
-    }
+    // Verify logic for below function. May not be needed
+    // function withdrawFunds() public onlyOwner isClosed
+    // {
+    //     payable(msg.sender).transfer(address(this).balance);
+    // }
 }
+
+
+
+/*
+
+    1) The owner can be the web application.
+    2) The owner can deploy the contract
+    3) The web application can read the global warming stats from some website
+    4) It can create an NFT and keep it for auction
+    5) If the tempoerature rises, it can close out the previous auction, payout the winner
+    6) Start again from point 4
+*/
